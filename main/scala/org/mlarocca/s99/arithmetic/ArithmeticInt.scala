9806eb3e368eda092a5c6d202eefbf31c235a4b8
mlarocca <marcellolarocca@gmail.com>
2015-12-02 20:56:46 +0000

    P34 | equality

--- main/scala/org/mlarocca/s99/arithmetic/ArithmeticInt.scala
+++ main/scala/org/mlarocca/s99/arithmetic/ArithmeticInt.scala
@@ -1,5 +1,7 @@
 package org.mlarocca.s99.arithmetic
 
+import javax.print.attribute.standard.MediaSize.Other
+
 import scala.annotation.tailrec
 import scala.util.Random
 
@@ -13,6 +15,18 @@ object ArithmeticInt {
 
 case class ArithmeticInt(value: Int) {
   import ArithmeticInt._
+
+  override def hashCode = value
+
+  override def canEqual(other: Any): Boolean = {
+    other.isInstanceOf[ArithmeticInt]
+  }
+
+  override def equals(other: Any) = other match {
+    case that: ArithmeticInt => (that canEqual this) && this.hashCode == that.hashCode
+    case thatValue: Int => this.value == thatValue
+    case _ => false
+  }
 
   @throws[IllegalArgumentException]
   def isPrime(): Boolean = value match {
@@ -28,8 +42,8 @@ case class ArithmeticInt(value: Int) {
   /**
    * Miller–Rabin primality test.
    *
-   * @param k How many iterations should be run. Precision ~= 2^(-k).
-   * @throws IllegalArgumentException
+   * @param k How many iterations should be run. Precision ~= 2**(-k).
+   * @throws IllegalArgumentException(NegativeValueErrorMessage)
    * @return
    */
   @throws[IllegalArgumentException]
@@ -39,7 +53,7 @@ case class ArithmeticInt(value: Int) {
     /**
      * Scala translation for:
      *  repeat r − 1 times:
-     *    x ← x^2 mod n
+     *    x ← x ** 2 mod n
      *    if x = 1 then return composite
      *    if x = n − 1 then break
      *  composite
@@ -87,6 +101,27 @@ case class ArithmeticInt(value: Int) {
     case ArithmeticInt(v) => v.gcd(value % v)
   }
 
+  /**
+   *
+   * @param other
+   * @return
+   */
+  def isCoprime(other: ArithmeticInt): Boolean = Math.abs(this.gcd(other)) == 1
+
+  /**
+   * Compute Euler's totient function.
+   * https://en.wikipedia.org/wiki/Euler's_totient_function
+   *
+   * @return
+   */
+  def totient(): Int = value match {
+    case 0 => 0
+    case i if i < 0  => Math.abs(i).totient()
+    case _ =>
+      1 + (2 to (value - 1)).map { i =>
+       if (this.gcd(i) == 1) 1 else 0
+      }.sum
+  }
 
   ////////////////////////////////////////////
   //              Utilities
@@ -94,7 +129,7 @@ case class ArithmeticInt(value: Int) {
 
 
   /**
-   * Represent an int as the product 2^r * d
+   * Represent an int as the product 2**r * d
    * @param n
    * @return
    */
--- test/scala/org/mlarocca/s99/arithmetic/ArithmeticIntTest.scala
+++ test/scala/org/mlarocca/s99/arithmetic/ArithmeticIntTest.scala
@@ -8,27 +8,45 @@ import scala.util.Random
 class ArithmeticIntTest extends FunSpec with Matchers {
   private val IterationNumber = 1000
 
+  describe("equality") {
+    it ("should compare correctly Ints, Bytes, Longs etc... ") {
+      1 == ArithmeticIntToInt(1) should be(right = true)
+      1L == ArithmeticIntToInt(1) should be(right = true)
+      1.toByte == ArithmeticIntToInt(1) should be(right = true)
+      1.0 == ArithmeticIntToInt(1) should be(right = true)
+
+      1 == ArithmeticIntToInt(2) should be(right = false)
+      1L == ArithmeticIntToInt(2) should be(right = false)
+      1.toByte == ArithmeticIntToInt(2) should be(right = false)
+      1.0 == ArithmeticIntToInt(2) should be(right = false)
+    }
+
+    it ("should compare correctly two ArithmeticInts") {
+      ArithmeticIntToInt(1) == ArithmeticIntToInt(1) should be(right = true)
+      ArithmeticIntToInt(1) == ArithmeticIntToInt(2) should be(right = false)
+    }
+  }
   describe("isPrime") {
     it ("should throw IllegalArgumentException for non positive Int") {
       a[IllegalArgumentException] should be thrownBy {
         (-1).isPrime()
       }
       a[IllegalArgumentException] should be thrownBy {
-        (0).isPrime()
+        0.isPrime()
       }
     }
 
     it ("should return the correct value for positive Int") {
-      1.isPrime should be(false)
-      2.isPrime should be(true)
-      3.isPrime should be(true)
-      4.isPrime should be(false)
-      5.isPrime should be(true)
-      6.isPrime should be(false)
-      7.isPrime should be(true)
-      13.isPrime should be(true)
-      25.isPrime should be(false)
-      31.isPrime should be(true)
+      1.isPrime should be(right = false)
+      2.isPrime should be(right = true)
+      3.isPrime should be(right = true)
+      4.isPrime should be(right = false)
+      5.isPrime should be(right = true)
+      6.isPrime should be(right = false)
+      7.isPrime should be(right = true)
+      13.isPrime should be(right = true)
+      25.isPrime should be(right = false)
+      31.isPrime should be(right = true)
     }
   }
 
@@ -38,22 +56,22 @@ class ArithmeticIntTest extends FunSpec with Matchers {
         (-1).isProbablyPrime()
       }
       a[IllegalArgumentException] should be thrownBy {
-        (0).isProbablyPrime()
+        0.isProbablyPrime()
       }
     }
 
     it ("should return the correct value for positive Int") {
-      1.isProbablyPrime() should be(false)
-      2.isProbablyPrime() should be(true)
-      3.isProbablyPrime() should be(true)
-      4.isProbablyPrime() should be(false)
-      5.isProbablyPrime() should be(true)
-      6.isProbablyPrime() should be(false)
-      7.isProbablyPrime() should be(true)
-      13.isProbablyPrime() should be(true)
-      25.isProbablyPrime() should be(false)
-      26.isProbablyPrime() should be(false)
-      31.isProbablyPrime() should be(true)
+      1.isProbablyPrime() should be(right = false)
+      2.isProbablyPrime() should be(right = true)
+      3.isProbablyPrime() should be(right = true)
+      4.isProbablyPrime() should be(right = false)
+      5.isProbablyPrime() should be(right = true)
+      6.isProbablyPrime() should be(right = false)
+      7.isProbablyPrime() should be(right = true)
+      13.isProbablyPrime() should be(right = true)
+      25.isProbablyPrime() should be(right = false)
+      26.isProbablyPrime() should be(right = false)
+      31.isProbablyPrime() should be(right = true)
 
     }
 
@@ -101,7 +119,6 @@ class ArithmeticIntTest extends FunSpec with Matchers {
     }
 
     it ("should be simmetric [randomTest]") {
-
       (1 to IterationNumber).foreach{ _ =>
         val a = Random.nextInt()
         val b = Random.nextInt()
@@ -109,4 +126,58 @@ class ArithmeticIntTest extends FunSpec with Matchers {
       }
     }
   }
+
+  describe("isCoprime") {
+    it("should return the correct result for positive values") {
+      1.isCoprime(3) should be(right = true)
+      2.isCoprime(3) should be(right = true)
+      2.isCoprime(6) should be(right = false)
+      3.isCoprime(9) should be(right = false)
+      42.isCoprime(63) should be(right = false)
+      7.isCoprime(5) should be(right = true)
+      13.isCoprime(11) should be(right = true)
+    }
+
+    it("should return the correct result if one or both arguments are negative Ints") {
+      (-1).isCoprime(3) should be(right = true)
+      2.isCoprime(-3) should be(right = true)
+      (-2).isCoprime(6) should be(right = false)
+      (-3).isCoprime(-7) should be(right = true)
+      42.isCoprime(-63) should be(right = false)
+    }
+
+    it("should be true iff gcd is 1 [randomTest]") {
+
+      (1 to IterationNumber).foreach { _ =>
+        val a = Random.nextInt()
+        val b = Random.nextInt()
+        a.isCoprime(b) should be(Math.abs(b.gcd(a)) == 1)
+      }
+    }
+  }
+
+  describe("totient") {
+    it("should return the correct result for positive values") {
+      1.totient() should be(1)
+      2.totient() should be(1)
+      3.totient() should be(2)
+      7.totient() should be(6)
+      8.totient() should be(4)
+      9.totient() should be(6)
+      10.totient() should be(4)
+      42.totient() should be(12)
+    }
+
+    it("should return the correct result if one or both arguments are negative Ints") {
+      -1.totient() should be(1)
+      -2.totient() should be(1)
+      -3.totient() should be(2)
+      -7.totient() should be(6)
+      -8.totient() should be(4)
+      -9.totient() should be(6)
+      -10.totient() should be(4)
+      -42.totient() should be(12)
+    }
+  }
+  
 }
