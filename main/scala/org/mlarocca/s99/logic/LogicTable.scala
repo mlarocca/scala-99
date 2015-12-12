3a4fc7fa63ec16f4b25ad1d66beff14c688bb25b
mlarocca <marcellolarocca@gmail.com>
2015-12-12 18:27:52 +0000

    P50 Huffman

--- main/scala/org/mlarocca/s99/logic/LogicTable.scala
+++ main/scala/org/mlarocca/s99/logic/LogicTable.scala
@@ -4,14 +4,13 @@ import scala.collection.mutable
 import scala.collection.mutable.{Map => MutableMap}
 
 object LogicTable {
-  private[s99] val NegativeValueErrorMessage = "Value must be positive"
 
-  def and(a: Boolean, b: Boolean): Boolean = (a,b) match {
+  def and(a: Boolean, b: Boolean): Boolean = (a, b) match {
     case (true, true) => true
     case _ => false
   }
 
-  def or(a: Boolean, b: Boolean): Boolean = (a,b) match {
+  def or(a: Boolean, b: Boolean): Boolean = (a, b) match {
     case (false, false) => false
     case _ => true
   }
@@ -41,6 +40,78 @@ object LogicTable {
     or(not(a), b)
   }
 
+  implicit def LogicTableToBoolean(a: LogicTable): Boolean = a.value
+  implicit def BooleanToLogicTable(a: Boolean): LogicTable = LogicTable(a)
+}
+
+private object Codes {
+  
+  @throws[IllegalArgumentException]
+  val gray: (Int) => Seq[String] = memoizer({
+      case n: Int if (n <= 0) =>
+          throw new IllegalArgumentException(NegativeValueErrorMessage)
+      case n: Int =>
+        val base = gray(n - 1)
+        base.map("0" + _) ++ base.reverse.map("1" + _)
+    },
+    MutableMap(1 -> Seq("0", "1"))
+  )
+
+  /**
+   * Computes the Huffman code of a sequence of symbols, based on their frequencies.
+   * 
+   * @param symbolsFrequencies A sequence of Tuples: each symbol paired with its frequency.
+   * @tparam V The type of the input symbols.
+   * @return A list,of the symbols, in the same order as they appear in the input, paired with their Huffman encoding,
+   */
+  def huffman[V](symbolsFrequencies: Seq[(V, Int)]): Seq[(V, String)] = symbolsFrequencies match {
+    case Nil =>
+      Nil
+
+    case (s, _) :: Nil =>
+      Seq((s, "0"))
+
+    case _ =>
+
+      def updateMap(symbol: String)(accumMap: Map[V, String], pattern: V): Map[V, String] = {
+        val prevString = accumMap(pattern)
+        accumMap + ((pattern, prevString + symbol))
+      }
+
+      def huffmanR(frequenciesQueue: mutable.PriorityQueue[QueueElement[V]]): Map[V, String] = frequenciesQueue.size match {
+        case 0|1 => Map.empty.withDefaultValue("")
+        case _ =>
+          val e1 = frequenciesQueue.dequeue()
+          val e2 = frequenciesQueue.dequeue()
+          val newEl = QueueElement[V](e1.value ++ e2.value, e1.priority + e2.priority)
+
+          val leftBranchMap = e2.value.foldLeft(huffmanR(frequenciesQueue.+=(newEl)))(updateMap("1"))
+          e1.value.foldLeft(leftBranchMap)(updateMap("0"))
+      }
+
+      val symbolsTable = huffmanR(
+        symbolsFrequencies.foldLeft(
+          new mutable.PriorityQueue[QueueElement[V]]()(Ordering.by((_: QueueElement[V]).priority).reverse)
+        ){ (queue, f) =>
+          queue.+=(QueueElement(Seq(f._1), f._2))
+        }
+      )
+
+      symbolsFrequencies.map {
+        case (s, _) => (s, symbolsTable(s))
+      }
+  }
+
+  private[s99] val NegativeValueErrorMessage = "Value must be positive"
+  private case class QueueElement[V](value: Seq[V], priority: Int)
+
+  /**
+   * Turn a function f into a memoized function, checking local cache before computing its value.
+   * 
+   * @param f
+   * @param cache
+   * @return
+   */
   private def memoizer(
     f: Int => Seq[String],
     cache: MutableMap[Int, Seq[String]] = MutableMap.empty
@@ -49,60 +120,10 @@ object LogicTable {
     (i: Int) => {
       if (!cache.contains(i)) {
         cache.+=((i, f(i)))
-        println(cache)
       }
       cache(i)
     }
-  }
-
-  @throws[IllegalArgumentException]
-  val gray: (Int) => Seq[String] = memoizer({
-      case n: Int if (n <= 0) =>
-          throw new IllegalArgumentException(NegativeValueErrorMessage)
-      case n: Int =>
-        println(s"computing grayR($n)")
-        val base = gray(n - 1)
-        base.map("0" + _) ++ base.reverse.map("1" + _)
-    },
-    MutableMap(1 -> Seq("0", "1"))
-  )
-
-  private case class QueueElement(value: Seq[String], priority: Int)
-
-  def huffman(frequencies: Seq[(String, Int)]): Seq[(String, String)] = {
-    def huffmanR(frequenciesQueue: mutable.PriorityQueue[QueueElement]): Map[String, String] = frequenciesQueue.size match {
-      case 0 => Map.empty
-      case 1 => frequenciesQueue.head.value.map{ (_ -> "") }.toMap.withDefaultValue("")
-      case _ =>
-        val e1 = frequenciesQueue.dequeue()
-        val e2 = frequenciesQueue.dequeue()
-        val newEl = QueueElement(e1.value ++ e2.value, e1.priority + e2.priority)
-        frequenciesQueue.+=(newEl)
-        val lMap = e2.value.foldLeft(huffmanR(frequenciesQueue)) {
-          case (hMap, s) =>
-            val prevString = hMap(s)
-            hMap + ((s, prevString + "1"))
-        }
-        e1.value.foldLeft(lMap) {
-          case (hMap, s) =>
-            val prevString = hMap(s)
-            hMap + ((s, prevString + "0"))
-        }
-
-    }
-
-    val pq = new mutable.PriorityQueue[QueueElement]()(Ordering.by((_: QueueElement).priority).reverse)
-    val symbolsTable = huffmanR(frequencies.foldLeft(pq){ (queue, f) =>
-      pq.+=(QueueElement(Seq(f._1), f._2))
-    })
-
-    frequencies.map {
-      case (s, _) => (s, symbolsTable(s))
-    }
-  }
-
-  implicit def LogicTableToBoolean(a: LogicTable): Boolean = a.value
-  implicit def BooleanToLogicTable(a: Boolean): LogicTable = LogicTable(a)
+  }  
 }
 
 case class LogicTable(value:  Boolean) {
